b7404ad80bf45402e7f48919af82a5108f965edd
Eduardo Lazzaretti <duadsal@gmail.com>
2024-01-16 19:50:47 -0300

    First commit

---
+++ includes/form.php
@@ -0,0 +1,42 @@
+<div class="jumbotron bg-white text-dark">
+
+    <h2>Esconder seu Texto</h2>
+
+    <form method="post" enctype="multipart/form-data">
+
+        <div class="form-group">
+            <label>Image</label>
+            <input type="file" name="image" class="form-control">
+        </div>
+
+        <div class="form-group">
+            <label>Text</label>
+            <input type="text" name="text" class="form-control">
+        </div>
+
+        <div class="form-group">
+            <button name="esconder" class="btn btn-success">Esconder Mensagem</button>
+        </div>
+
+    </form>
+</div> 
+
+<hr class="border-light">
+
+<div class="jumbotron bg-white text-dark">
+
+    <h2>Ler seu Texto</h2>
+
+    <form method="post" enctype="multipart/form-data">
+
+        <div class="form-group">
+            <label>Image</label>
+            <input type="file" name="image" class="form-control">
+        </div>
+
+        <div class="form-group">
+            <button name="ler" class="btn btn-success">Ler Mensagem</button>
+        </div>
+
+    </form>
+</div> 
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+if(isset($_POST['esconder']))
+{
+    
+}
+
+include_once __DIR__ . '/includes/header.php';
+include_once __DIR__ . '/includes/form.php';
+include_once __DIR__ . '/includes/footer.php';
