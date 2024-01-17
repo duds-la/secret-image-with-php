f6169bf7ced9bce7c954c80cbe871836ba6b05cb
Eduardo Lazzaretti <duadsal@gmail.com>
2024-01-17 19:39:30 -0300

    Class Image

--- app/File/Image.php
+++ app/File/Image.php
@@ -4,5 +4,32 @@ namespace App\File;
 
 class Image
 {
-    
+    /**
+     * Resource de imagem da bib. GD
+     * @var resource
+     */
+    private $image;
+
+    private $name;
+
+    private $type;
+
+    private $width;
+
+    private $height;
+
+    public function __construct($file)
+    {
+        //DADOS DA IMAGEM
+        $this->name = $file['name'];
+        $this->type = $file['type'];
+
+        //RESOURCE DA BIBLIOTECA GD
+        $this->image = imagecreatefromstring(file_get_contents($file['tmp_name']));
+
+        //ALTURA E LARGURA
+        list($this->width,$this->height) = getimagesize($file['tmp_name']);
+
+
+    }
 }
--- index.php
+++ index.php
@@ -2,9 +2,11 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
+use \App\File\Image;
+
 if(isset($_POST['esconder']))
 {
-    
+    $obImage = new Image($_FILES['imagem']);
 }
 
 include_once __DIR__ . '/includes/header.php';
